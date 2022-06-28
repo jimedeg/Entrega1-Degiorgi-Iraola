af6ac4c92d3bda0e248bec20e86698e2bb89d5a6
jimedeg <jimena.degiorgi@gmail.com>
2022-06-27 23:45:25 -0300

    creando vistas y urls

---
+++ proyecto_curso/proyecto_cursoApp/urls.py
@@ -0,0 +1,28 @@
+"""proyecto_curso URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path ('', inicio, name= "inicio" ),
+    path ('curso', curso, name= "curso"),
+    path ('evento', evento, name= "evento"),
+    path ('contacto', contacto, name= "contacto"),
+    path ('crear_curso', crear_curso, name= "crear_curso"),
+    path ('crear_evento', crear_evento, name= "crear_evento"),
+    path ('busqueda', busqueda, name= "busqueda"),
+    ]
---
+++ proyecto_curso/proyecto_cursoApp/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+
+from proyecto_cursoApp.models import Curso, Evento
+from .forms import nuevo_curso
+from django.db.models import Q
+
+# Create your views here.
+def inicio (request):
+    
+    curso = Curso.objects.all()[:3]
+    evento = Evento.objects.all()[:3]
+    
+    return render(request,"proyecto_cursoApp/index.html",{'curso': curso , 'evento': evento})
+
+
+def curso (request):
+    
+    curso = Curso.objects.all()
+            
+    return render(request,"proyecto_cursoApp/curso.html",{'curso': curso})
+
+def evento (request):
+    
+    evento = Evento.objects.all()
+    
+    return render(request,"proyecto_cursoApp/evento.html",{'evento': evento})
+
+def contacto (request):
+    
+    return render(request,"proyecto_cursoApp/contacto.html",{})
+
+def crear_curso (request):
+    #post
+    if request.method == "POST":
+        
+        formulario = nuevo_curso(request.POST)
+        
+        if formulario.is_valid():
+            
+            info_curso = formulario.cleaned_data
+            
+            curso = Curso(nombre = info_curso ["nombre"], info = info_curso ["informacion"], fecha = info_curso ["fecha"] )
+            curso.save() #Guardar en la db
+            
+            return redirect("curso")
+        else:
+            return render(request,"proyecto_cursoApp/crear_curso.html",{"form":formulario})
+    else:
+        
+        formulario_vacio= nuevo_curso()
+         
+        return render(request,"proyecto_cursoApp/crear_curso.html",{"form":formulario_vacio})
+
+def crear_evento (request):
+    #post
+    if request.method == "POST":
+        
+        form = nuevo_evento(request.POST)
+        
+        if form.is_valid():
+            
+            info_evento = form.cleaned_data
+            
+            evento = Evento(nombre = info_evento ["nombre"], info = info_evento ["informacion"], fecha = info_evento ["fecha"] )
+            evento.save() #Guardar en la db
+            
+            return redirect("evento")
+        else:
+            return render(request,"proyecto_cursoApp/crear_evento.html",{"form":form})
+    else:
+        
+        form_vacio= nuevo_evento()
+         
+        return render(request,"proyecto_cursoApp/crear_evento.html",{"form":form_vacio})
+               
+def busqueda (request):
+         
+    if request.method == "POST":
+        
+        nombre = request.POST["nombre"] 
+             
+        busquedas = Curso.objects.filter(nombre__icontains=nombre)
+        
+        return render(request,"proyecto_cursoApp/busqueda.html",{"busquedas": busquedas})
+  
+    else:
+        
+        busquedas=[]
+        
+        return render(request,"proyecto_cursoApp/busqueda.html",{"busquedas": busquedas})
+       
